73da565c05f293d77981706bc6a00a190d5eb05c
Ilya <ahitkinilyapavlovich@gmail.com>
2023-05-23 22:34:05 +0500

    Добавил папку конфиг

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,19 @@
+import webpack from 'webpack'
+import HTMLWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BuildOptions } from './types/config'
+
+export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
+    const { html } = paths
+
+    return [
+        new webpack.ProgressPlugin(),
+        new HTMLWebpackPlugin({
+            template: html,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+    ]
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,28 @@
+import webpack from 'webpack'
+import { BuildOptions } from './types/config'
+import { buildPlugins } from './buildPlugins'
+import { buildLoaders } from './buildLoaders'
+import { buildResolvers } from './buildResolvers'
+import { buildDevServer } from './buildDevServer'
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { paths, mode, isDev } = options
+    const { build, entry } = paths
+
+    return {
+        mode,
+        entry: entry,
+        output: {
+          filename: '[name].[contenthash].js',
+          path: build,
+          clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+          rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    }
+}
